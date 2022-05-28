fe8641c969b172468b8238750a34fe44f71175c9
ptso <prestonmtso@gmail.com>
2022-05-28 15:03:01 -0700

    form input

--- library.js
+++ library.js
@@ -1,7 +1,21 @@
+let myLibrary =[]
+
 function createBook() {
     var bookData = new FormData()
     bookData.append("title",document.getElementById("Title").value)
     bookData.append("author",document.getElementById("Author").value)
-    alert(bookData)
+    bookData.append("pages",document.getElementById("Pages").value)
+    const readRadio = document.querySelector("input[name=have_read]:checked").value
+    bookData.append("read",readRadio)
+    const bookObject = JSON.stringify(Object.fromEntries(bookData)) 
+    myLibrary.push(bookObject)
+    const container = document.getElementById("container")
+    const div = document.createElement("div")
+    div.textContent = myLibrary
+    container.appendChild(div)
 }
 
+function displayLibrary() {
+    createBook()
+    alert(myLibrary)
+}
