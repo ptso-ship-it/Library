13faa1c0367b31753f1605af811ee83a86869e93
ptso <prestonmtso@gmail.com>
2022-05-31 23:07:19 -0700

    working library

--- library.js
+++ library.js
@@ -2,14 +2,44 @@ var form = document.getElementById("form")
 form.addEventListener("submit", createBook)
 
 var myLibrary =[]
+var displayedLibrary = []
+
+function addRow(title, author) {
+    var tbodyRef = document.getElementById("bookTable").getElementsByTagName("tbody")[0]
+    var newRow = tbodyRef.insertRow()
+    newRow.classList.add("newRow")
+    var titleCell = newRow.insertCell()
+    var authorCell = newRow.insertCell()
+    var readCell = newRow.insertCell()
+    var removeCell = newRow.insertCell()
+    var titleText = document.createTextNode(title)
+    var authorText = document.createTextNode(author)
+    var readText = document.createElement("INPUT")
+    readText.setAttribute("type", "checkbox")
+    const remove = document.createElement('button');
+    const handleRemove = e => {
+        const item = e.target.closest('.newRow');
+        item.parentElement.removeChild(item);
+      };
+    remove.textContent = 'Remove';
+    remove.addEventListener('click', handleRemove);
+    // document.createTextNode(displayedLibrary.map(x => x.value));
+    titleCell.appendChild(titleText)
+    authorCell.appendChild(authorText)
+    readCell.appendChild(readText)
+    removeCell.appendChild(remove)
+    // document.getElementById("container").innerHTML = displayedLibrary
+}
 
 function createBook(event) {
     var bookData = new FormData(event.target)
     bookData.append("title", document.getElementById("title").value)
     bookData.append("author", document.getElementById("author").value)
-    const bookObject = JSON.stringify(Object.fromEntries(bookData)) 
-    myLibrary.push(bookObject)
-    document.getElementById("container").innerHTML = myLibrary
+    addRow(bookData.get("title"), bookData.get("author"))
+    // const bookObject = JSON.stringify(Object.fromEntries(bookData)) 
+    // myLibrary.push(bookData)
+    // displayedLibrary.push(bookObject)
+    
     event.preventDefault()
 }
 
